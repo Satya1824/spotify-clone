e9b7676650f5f9adf854bc0fde7f6db9f95341b0
Satya Prakash <satsou24@gmail.com>
2024-01-25 21:39:54 +0530

    Update AuthModal.tsx

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -42,7 +42,7 @@ const AuthModal = () => {
     >
       <Auth
         supabaseClient={supabaseClient}
-        providers={["github", "facebook", "google"]}
+        providers={["github"]}
         magicLink={true}
         appearance={{
           theme: ThemeSupa,
