e4f3fbaa8f182c176aa79ed623e6a40bffb9380b
Satya Prakash <satsou24@gmail.com>
2024-01-25 09:44:43 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["whafrtjgjdkecvndukqp.supabase.co"],
+  },
+};
 
 export default nextConfig;
